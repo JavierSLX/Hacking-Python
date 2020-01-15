756436ecc551388658843fa27f8291a9a4c0d962
JavierSLX <jserranolule@gmail.com>
2020-01-14 22:26:30 -0600

    Fin de la seccion 3

---
+++ Seccion_3/EntradaDeDatos.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+nombre = raw_input("Digite su nombre: ")
+edad = int(raw_input("Digite la edad: "))
+print("Hola {}, tienes {} años".format(nombre, edad)) 
